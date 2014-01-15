b2776f6a9841c57cdda29b7989e8bd62a583c59c
Micah Godbolt <micahgodbolt@gmail.com>
2014-01-15 14:15:22 -0800

    slider and other cleanup

--- index.php
+++ index.php
@@ -13,41 +13,43 @@
       <link rel="stylesheet" href="stylesheets/screen.css">
   </head>
   <body>
-    <div class="page">
-      <header>
-        <?php include 'partials/header.php' ?>
-      </header> <!-- end header -->
-      <nav class="main-navigation">
-        <?php include 'partials/main-navigation.php' ?>
-      </nav> <!-- end nav -->
+    <div class="wrapper">
+      <div class="page">
+        <header>
+          <?php include 'partials/header.php' ?>
+        </header> <!-- end header -->
+        <nav class="main-navigation">
+          <?php include 'partials/main-navigation.php' ?>
+        </nav> <!-- end nav -->
 
-      <section class="hero">
-       <?php include 'partials/hero-slider.php' ?>
-       <?php include 'partials/ad-block.php' ?>
+        <section class="hero">
+         <?php include 'partials/hero-slider.php' ?>
+         <?php include 'partials/ad-block.php' ?>
 
-      </section> <!-- end .hero -->
+        </section> <!-- end .hero -->
 
-      <section class="content">
-        <div class="main-content">
-          <?php include 'partials/browse-categories.php' ?>
-          <?php include 'partials/recent-articles.php' ?>
-          <?php include 'partials/popular-videos.php' ?>
-        </div> <!-- end .main-content -->
-        <div class="secondary-content">
-          <?php include 'partials/newsletter-signup.php' ?>
-          <?php include 'partials/current-contests.php' ?>
-          <?php include 'partials/twitter-widget.php' ?>
-        </div> <!-- end .secondary-content -->
-      </section> <!-- end .content -->
+        <section class="content">
+          <div class="main-content">
+            <?php include 'partials/browse-categories.php' ?>
+            <?php include 'partials/recent-articles.php' ?>
+            <?php include 'partials/popular-videos.php' ?>
+          </div> <!-- end .main-content -->
+          <div class="secondary-content">
+            <?php include 'partials/newsletter-signup.php' ?>
+            <?php include 'partials/current-contests.php' ?>
+            <?php include 'partials/twitter-widget.php' ?>
+          </div> <!-- end .secondary-content -->
+        </section> <!-- end .content -->
 
-      <footer>
-        <?php include 'partials/privacy-terms-copyright.php' ?>
-        <?php include 'partials/social-links.php' ?>
-      </footer> <!-- end footer -->
+        <footer>
+          <?php include 'partials/privacy-terms-copyright.php' ?>
+          <?php include 'partials/social-links.php' ?>
+        </footer> <!-- end footer -->
 
 
 
-    </div>  <!-- end .page -->
+      </div>  <!-- end .page -->
+    </div> <!-- end .wrapper -->
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
     <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
     <script src="js/plugins.js"></script>
---
+++ js/main.js
@@ -0,0 +1,38 @@
+$( document ).ready(function() {
+  var currentSlide = 1;
+
+  function changeSlideTo(slideNumber) {
+    $('.slider-items .active').removeClass('active');
+    $(".slider-items .slider-item:nth-child(" + slideNumber + ")").addClass('active');
+    $('.slider-navigation .active').removeClass('active');
+    $(".slider-navigation li:nth-child(" + slideNumber + ")").addClass('active');
+  }
+
+  function incrementSlide () {
+    if (currentSlide == 4) {
+      currentSlide = 1;
+    }
+    else {
+      currentSlide = currentSlide + 1;
+    }
+    console.log(currentSlide);
+    changeSlideTo(currentSlide);
+  }
+
+  $('.slider-navigation .slider-nav-link').on('click', function(e) {
+    e.preventDefault();
+    $this = $(this);
+    navNumber = $this.data("slide-number");
+    currentSlide = navNumber;
+    changeSlideTo(currentSlide);
+  });
+
+  setInterval(function(){
+    var isHovered = $('.hero').is(":hover"); // returns true or false
+    if (!isHovered) {
+        incrementSlide();
+    };
+  }, 6000);
+
+
+});
